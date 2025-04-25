b9b4dff4caa0552225751a6b201af2b646cd5d8f
TwiinArmageddon <YMDAYLIST@GMAIL.COM>
2025-04-26 00:33:36 +0200

    Add files via upload

--- error/scripts.js
+++ error/scripts.js
@@ -1,19 +1,27 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const enlace = document.getElementById('texto');
-  
-    enlace.addEventListener('click', function (e) {
-      e.preventDefault(); // Previene que siga el enlace
-  
-      if (enlace.textContent === "Pulsame otra vez y te mato") {
-        enlace.textContent = 'Ya está, me enfadé, ya no vuelvo a cambiar.';
-      } else if (enlace.textContent === "Deberías dejar de gastar energías.") {
-        enlace.textContent = 'Pulsame otra vez y te mato';
-      } else if (enlace.textContent === "Por que sigues?") {
-        enlace.textContent = 'Deberías dejar de gastar energías.';
-      } else if (enlace.textContent === "JiJi") {
-        enlace.textContent = 'Por que sigues?';
-      } else if (enlace.textContent === "Esta página no existe") {
-        enlace.textContent = 'JiJi';
-      }
-    });
+  const enlace = document.getElementById('texto');
+  const container = document.getElementById('container');
+  const imagen = document.getElementById('puerta');
+
+  enlace.addEventListener('click', function (e) {
+    e.preventDefault(); // Previene que siga el enlace
+
+    if (enlace.textContent === "Maybe I'm not completely tired, but you'll never know if you don't click on me.") {
+      container.style.display = "none";
+      imagen.style.display = "block";
+      imagen.style.animation = "abrirPuerta 3s ease-out forwards";
+    } else if (enlace.textContent === "That's it, I'm angry, I'm not going back.") {
+      enlace.textContent = "Maybe I'm not completely tired, but you'll never know if you don't click on me.";
+    } else if (enlace.textContent === "Click me again and I'll kill you.") {
+      enlace.textContent = "That's it, I'm angry, I'm not going back.";
+    } else if (enlace.textContent === "You should stop wasting energy.") {
+      enlace.textContent = "Click me again and I'll kill you.";
+    } else if (enlace.textContent === "Why do you continue?") {
+      enlace.textContent = 'You should stop wasting energy.';
+    } else if (enlace.textContent === "JiJi") {
+      enlace.textContent = 'Why do you continue?';
+    } else if (enlace.textContent === "This page doesn't exist") {
+      enlace.textContent = 'JiJi';
+    }
   });
+});
